90161d43200546dc0f95d27ea40f8af9df28fe01
galulex <galulex@gmail.com>
2026-03-05 18:04:50 +0200

    Adds configurable widgets

--- wf2/watchface/digits.js
+++ wf2/watchface/digits.js
@@ -7,8 +7,15 @@ import { pos } from './utils.js'
 
 var lastH = -1, lastM = -1
 var hDig0, hDig1, mDig0, mDig1
+var isDynamic = true
 
-export function placeDigits() {
+export function resetTime() {
+  lastH = -1
+  lastM = -1
+}
+
+export function placeDigits(dynamicTime) {
+  isDynamic = dynamicTime !== false
   hDig0 = hmUI.createWidget(hmUI.widget.IMG, { src: 'time/0.png', show_level: hmUI.show_level.ONLY_NORMAL })
   hDig1 = hmUI.createWidget(hmUI.widget.IMG, { src: 'time/0.png', show_level: hmUI.show_level.ONLY_NORMAL })
   mDig0 = hmUI.createWidget(hmUI.widget.IMG, { src: 'time-min/0.png', show_level: hmUI.show_level.ONLY_NORMAL })
@@ -43,42 +50,73 @@ export function placeTime() {
 
   var is24 = getTimeFormat() === TIME_FORMAT_24
   var displayHour = is24 ? h : (h % 12 || 12)
-  var hAngle = ((h % 12) * 30 - 90) * Math.PI / 180
-  var hR = px(155)
-  var hOx = Math.round(hR * Math.cos(hAngle))
-  var hOy = Math.round(hR * Math.sin(hAngle))
   var hDigitH = px(62)
   var hTens = Math.floor(displayHour / 10)
   var hOnes = displayHour % 10
   var hW = [60,40,52,52,52,52,56,46,55,56]
   var gap = px(10) * -1
 
-  if (hTens > 0) {
-    var w0 = px(hW[hTens])
-    var w1 = px(hW[hOnes])
-    var total = w0 + gap + w1
-    var p0 = pos(hOx - Math.floor(total / 2) + Math.floor(w0 / 2), hOy, w0, hDigitH)
-    var p1 = pos(hOx - Math.floor(total / 2) + w0 + gap + Math.floor(w1 / 2), hOy, w1, hDigitH)
-    hDig0.setProperty(hmUI.prop.MORE, { x: p0.x, y: p0.y, src: 'time/' + hTens + '.png' })
-    hDig1.setProperty(hmUI.prop.MORE, { x: p1.x, y: p1.y, src: 'time/' + hOnes + '.png' })
+  if (isDynamic) {
+    // Hours orbit at clock position
+    var hAngle = ((h % 12) * 30 - 90) * Math.PI / 180
+    var hR = px(155)
+    var hOx = Math.round(hR * Math.cos(hAngle))
+    var hOy = Math.round(hR * Math.sin(hAngle))
+
+    if (hTens > 0) {
+      var w0 = px(hW[hTens])
+      var w1 = px(hW[hOnes])
+      var total = w0 + gap + w1
+      var p0 = pos(hOx - Math.floor(total / 2) + Math.floor(w0 / 2), hOy, w0, hDigitH)
+      var p1 = pos(hOx - Math.floor(total / 2) + w0 + gap + Math.floor(w1 / 2), hOy, w1, hDigitH)
+      hDig0.setProperty(hmUI.prop.MORE, { x: p0.x, y: p0.y, src: 'time/' + hTens + '.png' })
+      hDig1.setProperty(hmUI.prop.MORE, { x: p1.x, y: p1.y, src: 'time/' + hOnes + '.png' })
+    } else {
+      var w = px(hW[hOnes])
+      var p = pos(hOx, hOy, w, hDigitH)
+      hDig0.setProperty(hmUI.prop.MORE, { x: p.x, y: p.y, src: 'time/' + hOnes + '.png' })
+      hDig1.setProperty(hmUI.prop.MORE, { x: -500, y: -500 })
+    }
+
+    // Minutes orbit at clock position
+    var mAngle = (m * 6 - 90) * Math.PI / 180
+    var mR = Math.floor(size / 2) - px(38)
+    var mOx = Math.round(mR * Math.cos(mAngle))
+    var mOy = Math.round(mR * Math.sin(mAngle))
+    var mDigitW = px(46)
+    var mDigitH = px(52)
+    var mGap = px(15) * -1
+    var mTotal = mDigitW * 2 + mGap
+
+    var mp0 = pos(mOx - Math.floor(mTotal / 2) + Math.floor(mDigitW / 2), mOy, mDigitW, mDigitH)
+    var mp1 = pos(mOx - Math.floor(mTotal / 2) + mDigitW + mGap + Math.floor(mDigitW / 2), mOy, mDigitW, mDigitH)
+    mDig0.setProperty(hmUI.prop.MORE, { x: mp0.x, y: mp0.y, src: 'time-min/' + Math.floor(m / 10) + '.png' })
+    mDig1.setProperty(hmUI.prop.MORE, { x: mp1.x, y: mp1.y, src: 'time-min/' + (m % 10) + '.png' })
+
   } else {
-    var w = px(hW[hOnes])
-    var p = pos(hOx, hOy, w, hDigitH)
-    hDig0.setProperty(hmUI.prop.MORE, { x: p.x, y: p.y, src: 'time/' + hOnes + '.png' })
-    hDig1.setProperty(hmUI.prop.MORE, { x: -500, y: -500 })
+    // Static: hours and minutes centered on screen
+    var cx = Math.floor(width / 2)
+    var cy = Math.floor(height / 2)
+    var hVOff = px(38)   // hours above center
+    var mVOff = px(10)   // minutes below center
+
+    if (hTens > 0) {
+      var sw0 = px(hW[hTens])
+      var sw1 = px(hW[hOnes])
+      var stotal = sw0 + gap + sw1
+      hDig0.setProperty(hmUI.prop.MORE, { x: cx - Math.floor(stotal / 2), y: cy - hVOff - hDigitH, src: 'time/' + hTens + '.png' })
+      hDig1.setProperty(hmUI.prop.MORE, { x: cx - Math.floor(stotal / 2) + sw0 + gap, y: cy - hVOff - hDigitH, src: 'time/' + hOnes + '.png' })
+    } else {
+      var sw = px(hW[hOnes])
+      hDig0.setProperty(hmUI.prop.MORE, { x: cx - Math.floor(sw / 2), y: cy - hVOff - hDigitH, src: 'time/' + hOnes + '.png' })
+      hDig1.setProperty(hmUI.prop.MORE, { x: -500, y: -500 })
+    }
+
+    var smDigitW = px(46)
+    var smDigitH = px(52)
+    var smGap = px(15) * -1
+    var smTotal = smDigitW * 2 + smGap
+    mDig0.setProperty(hmUI.prop.MORE, { x: cx - Math.floor(smTotal / 2), y: cy + mVOff, src: 'time-min/' + Math.floor(m / 10) + '.png' })
+    mDig1.setProperty(hmUI.prop.MORE, { x: cx - Math.floor(smTotal / 2) + smDigitW + smGap, y: cy + mVOff, src: 'time-min/' + (m % 10) + '.png' })
   }
-
-  var mAngle = (m * 6 - 90) * Math.PI / 180
-  var mR = Math.floor(size / 2) - px(38)
-  var mOx = Math.round(mR * Math.cos(mAngle))
-  var mOy = Math.round(mR * Math.sin(mAngle))
-  var mDigitW = px(46)
-  var mDigitH = px(52)
-  var mGap = px(15) * -1
-  var mTotal = mDigitW * 2 + mGap
-
-  var mp0 = pos(mOx - Math.floor(mTotal / 2) + Math.floor(mDigitW / 2), mOy, mDigitW, mDigitH)
-  var mp1 = pos(mOx - Math.floor(mTotal / 2) + mDigitW + mGap + Math.floor(mDigitW / 2), mOy, mDigitW, mDigitH)
-  mDig0.setProperty(hmUI.prop.MORE, { x: mp0.x, y: mp0.y, src: 'time-min/' + Math.floor(m / 10) + '.png' })
-  mDig1.setProperty(hmUI.prop.MORE, { x: mp1.x, y: mp1.y, src: 'time-min/' + (m % 10) + '.png' })
 }
--- wf2/watchface/index.js
+++ wf2/watchface/index.js
@@ -1,6 +1,6 @@
 import { delegate, editable } from '../../pages/ui.js'
 import * as hmUI from '@zos/ui'
-import { placeDigits, placeTime } from './digits.js'
+import { placeDigits, placeTime, resetTime } from './digits.js'
 import { placeSecondsPointer } from './seconds.js'
 import { placeStatusIcons } from './status.js'
 import { Heart } from './heart.js'
@@ -15,7 +15,6 @@ import { Pai } from './pai.js'
 import { Standing } from './standing.js'
 import { placeZones } from './zones.js'
 var timerId
-var timeoutId
 
 WatchFace({
   onInit() {},
@@ -23,9 +22,13 @@ WatchFace({
     // Labels on/off editable toggle (edit_id: 1)
     var labelToggle = editable({
       edit_id: 1,
-      x: 0, y: 0, w: 60, h: 60,
+      x: -100, y: 0, w: 150, h: 100,
       select_image: 'edit/labels-select.png',
       un_select_image: 'edit/labels-unselect.png',
+      tips_BG: 'edit/tips-bg.png',
+      tips_x: 0,
+      tips_y: 0,
+      tips_width: 150,
       optional_types: [
         { type: hmUI.edit_type.STEP,        preview: 'edit/labels-on.png' },
         { type: hmUI.edit_type.ALTIMETER,   preview: 'edit/labels-off.png' },
@@ -39,6 +42,27 @@ WatchFace({
       || currentType === hmUI.edit_type.STEP
       || currentType === hmUI.edit_type.HEART
 
+    // Dynamic/Static Time editable toggle (edit_id: 2)
+    var timeToggle = editable({
+      edit_id: 2,
+      x: 100, y: 0, w: 150, h: 100,
+      select_image: 'edit/labels-select.png',
+      un_select_image: 'edit/labels-unselect.png',
+      tips_BG: 'edit/tips-bg.png',
+      tips_x: 0,
+      tips_y: 0,
+      tips_width: 150,
+      optional_types: [
+        { type: hmUI.edit_type.CAL,         preview: 'edit/time-dynamic.png' },
+        { type: hmUI.edit_type.PAI_WEEKLY,  preview: 'edit/time-static.png' },
+        { type: hmUI.edit_type.CAL,         preview: 'edit/time-dynamic.png' },
+        { type: hmUI.edit_type.PAI_WEEKLY,  preview: 'edit/time-static.png' },
+      ],
+    })
+    var timeType = timeToggle.getProperty(hmUI.prop.CURRENT_TYPE)
+    // Dynamic = CAL (default); Static = PAI_WEEKLY
+    var dynamicTime = !timeType || timeType !== hmUI.edit_type.PAI_WEEKLY
+
     // placeGlance()
     placeStatusIcons(labelsEnabled)
     heartWidget = Heart(labelsEnabled)
@@ -51,10 +75,10 @@ WatchFace({
     Calories(labelsEnabled)
     Pai(labelsEnabled)
     Standing(labelsEnabled)
-    placeSecondsPointer()
+    if (dynamicTime) placeSecondsPointer()
 
     // Create and update digits last so they are on top
-    placeDigits()
+    placeDigits(dynamicTime)
     placeTime()
     // updateGlance()
 
@@ -62,18 +86,11 @@ WatchFace({
       placeTime()
       heartWidget.update()
 
-      if (timerId) { clearInterval(timerId); timerId = null }
-      if (timeoutId) { clearTimeout(timeoutId); timeoutId = null }
+      if (timerId) { timer.stopTimer(timerId); timerId = null }
 
-      // align to next minute boundary, then tick every 60s
-      var delay = (60 - new Date().getSeconds()) * 1000
-      timeoutId = setTimeout(function () {
-        placeTime()
-        timerId = setInterval(function () { placeTime() }, 60000)
-      }, delay)
+      timerId = timer.createTimer(0, 1000, function () { resetTime(); placeTime() }, {})
     }, function () {
-      if (timerId) { clearInterval(timerId); timerId = null }
-      if (timeoutId) { clearTimeout(timeoutId); timeoutId = null }
+      if (timerId) { timer.stopTimer(timerId); timerId = null }
     })
 
     placeZones()
