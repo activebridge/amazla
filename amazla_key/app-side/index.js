533453237452b2421a64c0306875e096dface1d7
galulex <galulex@gmail.com>
2026-04-10 22:56:52 +0300

    Removes http related app-side

--- amazla_key/app-side/index.js
+++ amazla_key/app-side/index.js
@@ -1,6 +1,4 @@
 import { BaseSideService } from '@zeppos/zml/base-side';
-import Auth from '../../app-side/tesla/auth'
-import Api from '../../app-side/tesla/api'
 import { store } from '../../app-side/tesla/utils'
 import TeslaSession from '../../app-side/tesla/session'
 import bleCrypto from './ble-crypto.js'
@@ -15,9 +13,6 @@ const dispatch = async (method, response, params = {}) => {
     if (func) {
       const result = await func(params)
       response(null, result)
-    } else {
-      const { status } = await Api[camalize(method)]()
-      response(null, { vehicle: store.vehicle, error: status })
     }
   } catch (e) {
     response(null, { success: false, error: e && e.message || 'dispatch error' })
@@ -72,15 +67,6 @@ const actions = {
     }
   },
 
-  VEHICLE_DATA: async() => {
-    let { status } = await Api.vehicleData()
-
-    if ([401, 403].includes(status)) await Auth.refreshToken()
-    if (status === 408) await Api.wakeUp()
-    if (status) ({ status } = await Api.vehicleData())
-    return { vehicle: store.vehicle, error: status }
-  },
-
   // BLE Pairing - build message bytes for watch to send
   BLE_PAIR: async (params) => {
     return bleCrypto.buildPairMessage(params.publicKeyHex)
@@ -129,14 +115,6 @@ AppSideService(
     onInit() {
       settings.settingsStorage.setItem('debug', '')
       settings.settingsStorage.addListener('change', async ({ key, newValue, oldValue }) => {
-        if (key === 'code' && newValue) {
-          await Auth.fetchToken(newValue)
-          await Api.vehicles()
-        }
-
-        if (key === 'command' && newValue === 'vehicles') {
-          await dispatch('VEHICLE_DATA', () => {})
-        }
       })
     },
 
